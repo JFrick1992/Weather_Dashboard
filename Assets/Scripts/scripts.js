c990fa0e9df88b438c9eb8b125a8ae3db6e5cf96
Jacob Frick <jacobhfrick1992@gmail.com>
2020-02-13 14:17:06 -0800

    completed

--- Assets/Scripts/scripts.js
+++ Assets/Scripts/scripts.js
@@ -15,25 +15,51 @@ function addButton(city) {
     div.innerHTML = `<button class="col s10 offset-s1 btn red" value="${city}">${city}</button>`
     document.getElementById('button-row').append(div)
 }
+function formatDate(date) {
+    let dateArray = date.split('-')
+    return `${parseInt(dateArray[1])}/${parseInt(dateArray[2])}/${dateArray[0]}`
+}
 function getCityData(city) {
     let url = `https://api.weatherapi.com/v1/forecast.json?key=ff4f207a8ae5485cb4320106200402&q=${city}&days=6`
     fetch(url)
         .then(r => r.json())
         .then(cityData => {
-            if(typeof cityData.error !== 'undefined' ) {
+            if (typeof cityData.error !== 'undefined') {
                 console.log(cityData.error)
             } else {
-                if(!citysFound.includes(cityData.location.name)) {
+                if (!citysFound.includes(cityData.location.name)) {
                     addButton(cityData.location.name)
                     citysFound.push(cityData.location.name)
                 }
-                console.log(cityData)
+                let forecast = cityData.forecast.forecastday
+                document.getElementById('temp').innerHTML = `Temperature: ${cityData.current.temp_f}&#176F`
+                document.getElementById('uv-index').innerHTML = `UV Index: ${cityData.current.uv}`
+                document.getElementById('humidity').innerText = `Humidity: ${cityData.current.humidity}%`
+                document.getElementById('wind-speed').innerText = `Wind Speed: ${cityData.current.wind_mph} MPH`
+                document.getElementById('city-name').innerHTML = `${cityData.location.name}(${formatDate(forecast[0].date)})<img src="${`https:${cityData.current.condition.icon}`}" alt="cityData.current.condition.text">`
+                forecast.shift()
+                let html = ''
+                forecast.forEach(el => {
+                    html += `
+                    <div class="col xl2 l3 m5 s12">
+                        <div class="col-content blue mt-10">
+                            <div class="ml-10">
+                                <h5 id="date-day-1">${formatDate(el.date)}</h5>
+                                <img src="https:${el.day.condition.icon}" alt="" id="img-day-1">
+                                <p id="temp-day-1">Temperature high: ${(el.day.maxtemp_f)}&#176F</p>
+                                <p id="temp-day-1">Temperature low: ${(el.day.mintemp_f)}&#176F</p>
+                            </div>
+                        </div>
+                    </div>
+                    `
+                })
+                document.getElementById('forecast-row').innerHTML = html
                 document.getElementById('no-city').classList.add('hide')
                 document.getElementById('city-data').classList.remove('hide')
             }
         })
         .catch(e => {
-            console.log("Hello")
+
         })
 }
 
@@ -42,10 +68,10 @@ document.getElementById('icon_prefix2').addEventListener('keydown', (event) => {
         getSeachInfo()
     }
 })
-document.addEventListener('click', ({target}) => {
+document.addEventListener('click', ({ target }) => {
     if (target.id === 'search-icon') {
         getSeachInfo()
-    } else if(target.type === 'submit') {
+    } else if (target.type === 'submit') {
         getCityData(target.value)
     }
 })
