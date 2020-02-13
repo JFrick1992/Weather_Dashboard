bc6bda1f68fd6329a0ae77992daad4d709de4be9
Jacob Frick <jacobhfrick1992@gmail.com>
2020-02-13 11:25:12 -0800

    Layout done, partial api function call

--- Assets/Scripts/scripts.js
+++ Assets/Scripts/scripts.js
@@ -1,27 +1,50 @@
 
 
 let dataDone = false
-document.getElementById('icon_prefix2').addEventListener('keydown', (event) => {
-    if (event.keyCode === 13 || event.which === 13) {
-        let searchText = document.getElementById('icon_prefix2').value
-        document.getElementById('icon_prefix2').value =""
-        if(searchText.length > 0) {
-            let div = document.createElement('div')
-            div.classList.add('row')
-            div.innerHTML= `<button class="col s10 offset-s1 btn red">${searchText}</button>`
-            document.getElementById('button-row').append(div)
-            
-        }
+let citysFound = []
+function getSeachInfo() {
+    let searchText = document.getElementById('icon_prefix2').value
+    document.getElementById('icon_prefix2').value = ""
+    if (searchText.length > 0) {
+        getCityData(searchText)
     }
-})
-let j = null
-const request = async () => {
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=london&appid=0ad7c955ef09cff110010cc7dc458217')
-    const json = await response.json()
-    j = 1
-    j =json
+}
+function addButton(city) {
+    let div = document.createElement('div')
+    div.classList.add('row')
+    div.innerHTML = `<button class="col s10 offset-s1 btn red" value="${city}">${city}</button>`
+    document.getElementById('button-row').append(div)
+}
+function getCityData(city) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0ad7c955ef09cff110010cc7dc458217`
 
+    fetch(url)
+        .then(r => r.json())
+        .then(cityData => {
+            if (cityData.cod == '404') {
+                console.log(cityData.cod)
+            } else if (cityData.cod == '200') {
+                console.log(cityData.cod)
+                if(!citysFound.includes(cityData.name)) {
+                    addButton(cityData.name)
+                    citysFound.push(cityData.name)
+                }
+            }
+        })
+        .catch(e => {
+            console.log("Hello")
+        })
 }
 
-request()
+document.getElementById('icon_prefix2').addEventListener('keydown', (event) => {
+    if (event.keyCode === 13 || event.which === 13) {
+        getSeachInfo()
+    }
+})
+document.addEventListener('click', ({ target }) => {
+    if (target.id === 'search-icon') {
+        getSeachInfo()
+    }
+})
+
 
